c42ae3c990e1a0d417cae15a23e606c61b1f8040
Tayyab Hakroe <tayyabhakro2k21@gmail.com>
2025-04-18 09:16:13 -0700

    work work

--- Q27.js
+++ Q27.js
@@ -1,4 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
 var minimumOperations = function(nums) {
-    console.log(nums)
+    // Use a Set to store unique non-zero values
+    let unique = new Set();
+    
+    for (let num of nums) {
+        if (num !== 0) {
+            unique.add(num);
+        }
+    }
+    
+    // The number of unique non-zero values is the answer
+    return unique.size;
 };
-console.log(minimumOperations([1,5,0,3,5]))
