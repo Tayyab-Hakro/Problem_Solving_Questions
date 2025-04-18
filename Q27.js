ec98baadcc9d459a126bb29c71b3f87331a4a99f
Tayyab Hakroe <tayyabhakro2k21@gmail.com>
2025-04-18 15:53:49 -0700

    question solved

--- Q27.js
+++ Q27.js
@@ -1,17 +1,5 @@
-/**
- * @param {number[]} nums
- * @return {number}
- */
-var minimumOperations = function(nums) {
-    // Use a Set to store unique non-zero values
-    let unique = new Set();
-    
-    for (let num of nums) {
-        if (num !== 0) {
-            unique.add(num);
-        }
-    }
-    
-    // The number of unique non-zero values is the answer
-    return unique.size;
-};
+let unique = new Set();
+for (let num of nums) {
+    if (num !== 0) unique.add(num);
+}
+return unique.size; // that's our answer
