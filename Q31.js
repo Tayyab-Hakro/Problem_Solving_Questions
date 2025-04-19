402756d633249dfa082239761e8635e438edf1bd
Tayyab Hakroe <tayyabhakro2k21@gmail.com>
2025-04-19 02:34:33 -0700

    not solved any question yet

--- Q31.js
+++ Q31.js
@@ -1,8 +1,22 @@
-const arr = [[1,4], [3,9]]
-let unique = new Set()
-for(let [start ,emd] of arr){
-for(let i = start ; i<emd ;i++){
- unique.add(i)
+function encryptSum(nums) {
+    let totalSum = 0;
+
+    for (let num of nums) {
+        // Convert number to string to access individual digits
+        let strNum = num.toString();
+        console.log(strNum)
+        // Find the largest digit in the number
+        let maxDigit = Math.max(...strNum.split(''));
+
+        // Create the encrypted number by repeating the max digit for the length of the original number
+        let encryptedNum = parseInt(maxDigit.repeat(strNum.length));
+
+        // Add the encrypted number to the total sum
+        totalSum += encryptedNum;
+    }
+
+    return totalSum;
 }
-console.log(unique.size) 
-}
+
+// Example usage:
+console.log(encryptSum([10, 21, 31]));  // Output: 66
