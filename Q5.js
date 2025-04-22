fe2e69a71e1771ed3ee8e9224a40080a408d5df6
Tayyab Hakroe <tayyabhakro2k21@gmail.com>
2025-04-22 04:58:01 -0700

    Solved

--- Q5.js
+++ Q5.js
@@ -1,9 +1,12 @@
-let arr = [90,42,40,30.88]
-let stack = []
-for(let op of arr){
-if(op > 40 ){
-    stack.push(op)
-}
-}
-
-console.log(stack)
+/**
+ * @param {number[]} target
+ * @param {number[]} arr
+ * @return {boolean}
+ */
+var canBeEqual = function(target, arr) {
+    target.sort((a,b)=> a-b)
+   arr.sort((a,b) => a-b)
+    return target.every((val , i) => val=== arr[i])
+    };
+console.log(canBeEqual([1,2,3,4],  [2,4,1,3]))
+ console.log(canBeEqual([3,7,9],  [3,7,11]))
