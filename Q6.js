f46446ae679c5f3fd048c66118ec7c6da7790b86
Tayyab Hakroe <tayyabhakro2k21@gmail.com>
2025-04-23 06:38:13 -0700

    not solveing any questiion

--- Q6.js
+++ Q6.js
@@ -1,9 +1,19 @@
-/**
- * @param {number[]} nums1
- * @param {number[]} nums2
- * @param {number[]} nums3
- * @return {number[]}
- */
-var twoOutOfThree = function(nums1, nums2, nums3) {
-};
-console.log(twoOutOfThree([1,1,3,2], [2,3], [3]))
+function maximumUnits(boxTypes, truckSize) {
+    // Step 1: Sort by units per box, descending
+    boxTypes.sort((a, b) => b[1] - a[1]);
+    console.log(boxTypes +"box")
+    let totalUnits = 0;
+
+    // Step 2: Load boxes greedily
+    for (let [boxes, units] of boxTypes) {
+        let take = Math.min(truckSize, boxes); // take as many as possible
+        totalUnits += take * units;
+        truckSize -= take;
+        if (truckSize === 0) break; // truck is full
+    }
+
+    return totalUnits;
+}
+
+
+console.log(maximumUnits([[1,3],[2,2],[3,1]],  4))
