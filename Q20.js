4c77771c2cea5a8d2977f7a679f8940291c1e26a
Tayyab Hakroe <tayyabhakro2k21@gmail.com>
2025-04-26 04:43:58 -0700

    second question solved

--- Q20.js
+++ Q20.js
@@ -1,2 +1,19 @@
-let arr = new Array(4)
-console.log(arr.push(5))
+/**
+ * @param {string[]} arr
+ * @param {number} k
+ * @return {string}
+ */
+var kthDistinct = function(arr, k) {
+let freq  = {}
+let res = []
+for(let char of arr){
+    freq[char] =  (freq[char] || 0) +1
+}
+for(let char in freq){
+if(freq[char] === 1){
+    res.push(char)
+}
+}
+return res[k-1] || ""
+};
+    console.log(kthDistinct( ["d","b","c","b","c","a"],  2 ))
