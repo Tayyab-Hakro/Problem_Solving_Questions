88ab561431eb5c04b38ab261cd353c650753ce9a
Tayyab Hakroe <tayyabhakro2k21@gmail.com>
2025-04-29 04:25:40 -0700

    question solved

--- Q11.js
+++ Q11.js
@@ -1,4 +1,21 @@
-let person = {name :"tayyab" , age:23}
+function findThePrefixCommonArray(A, B) {
+    const n = A.length;
+    const seenA = new Set();
+    const seenB = new Set();
+    const C = [];
+    let count = 0;
+    
+    for (let i = 0; i < n; i++) {
+        seenA.add(A[i]);
+        seenB.add(B[i]);
+        
+        if (seenA.has(B[i])) count++;
+        if (seenB.has(A[i]) && A[i] !== B[i]) count++;
+        
+        C.push(count);
+    }
+    
+    return C;
+}
 
-let {name,age} = person
-console.log(name ,age)
+console.log(findThePrefixCommonArray([1,3,2,4],  [3,1,2,4]))
